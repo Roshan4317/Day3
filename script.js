83deeed994df51c60813a20aeb1f2f076d266a4e
Roshan4317 <momlalsapkotanappal@gmail.com>
2026-02-02 23:58:18 -0500

    Daimond Pattern

--- script.js
+++ script.js
@@ -86,13 +86,59 @@
 // j===rows-i+1
 // Inverted Pyramid
 
-let rows = 5;
+// let rows = 5;
 
-for (let i = rows; i >= 1; i--) {
+// for (let i = rows; i >= 1; i--) {
+//   let star = "";
+
+//   for (let j = 1; j <= rows + i - 1; j++) {
+//     if (i === rows || j === rows - i + 1 || j === rows + i - 1) {
+//       star += "*";
+//     } else {
+//       star += " ";
+//     }
+//   }
+
+//   console.log(star);
+// }
+
+// ***********************************************************************************************************
+
+//ButterFly pattern
+
+// const rows = 5;
+
+// for (let i = 1; i <= rows * 2 - 1; i++) {
+//   let currentRow = i <= rows ? i : rows * 2 - i;
+
+//   let star = "";
+
+//   for (let j = 1; j <= rows * 2 - 1; j++) {
+//     if (j <= currentRow || j > rows * 2 - 1 - currentRow) {
+//       star += "*";
+//     } else {
+//       star += " ";
+//     }
+//   }
+
+//   console.log(star);
+// }
+
+// Print Diamond pattern
+
+// Print Diamond pattern
+
+const rows = 3;
+
+for (let i = 1; i <= rows * 2 - 1; i++) {
+  let currentRow = i <= rows ? i : rows * 2 - i;
   let star = "";
 
-  for (let j = 1; j <= rows + i - 1; j++) {
-    if (i === rows || j === rows - i + 1 || j === rows + i - 1) {
+  let left = rows - currentRow + 1;
+  let right = rows + currentRow - 1;
+
+  for (let j = 1; j <= rows * 2 - 1; j++) {
+    if (j >= left && j <= right) {
       star += "*";
     } else {
       star += " ";
@@ -101,3 +147,4 @@ for (let i = rows; i >= 1; i--) {
 
   console.log(star);
 }
+
