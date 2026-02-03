49939fc439cb5114e422d3b968065223e68177b0
Roshan4317 <momlalsapkotanappal@gmail.com>
2026-02-03 18:00:19 -0500

    Day 3 all Questions Done

--- script.js
+++ script.js
@@ -109,12 +109,10 @@
 // const rows = 5;
 
 // for (let i = 1; i <= rows * 2 - 1; i++) {
-//   let currentRow = i <= rows ? i : rows * 2 - i;
-
 //   let star = "";
 
 //   for (let j = 1; j <= rows * 2 - 1; j++) {
-//     if (j <= currentRow || j > rows * 2 - 1 - currentRow) {
+//     if (j <= i || j > rows * 2 - 1 - i) {
 //       star += "*";
 //     } else {
 //       star += " ";
@@ -124,27 +122,194 @@
 //   console.log(star);
 // }
 
-// Print Diamond pattern
+// const rows = 5;
+
+// for (let i = 1; i <= rows * 2 - 1; i++) {
+//   let star = "";
+
+//   let currentRow = i <= rows ? i : rows * 2 - i;
+
+//   for (let j = 1; j <= rows * 2 - 1; j++) {
+//     if (j <= currentRow || j >= rows * 2 - currentRow) {
+//       star += "*";
+//     } else {
+//       star += " ";
+//     }
+//   }
+
+//   console.log(star);
+// }
+
+// const row = 5;
+
+// for (let i = 1; i <= row * 2 - 1; i++) {
+//   let current = i <= row ? i : row * 2 - i;
+
+//   let frontStar = "*".repeat(current);
+//   let space = " ".repeat((row - current) * 2);
+//   let last = "*".repeat(current);
+
+//   console.log(frontStar + space + last);
+// }
+
+// ***********************************************************************************************************
 
 // Print Diamond pattern
 
-const rows = 3;
+// const rows = 3;
 
-for (let i = 1; i <= rows * 2 - 1; i++) {
-  let currentRow = i <= rows ? i : rows * 2 - i;
-  let star = "";
+// for (let i = 1; i <= rows * 2 - 1; i++) {
+//   let star = "";
 
-  let left = rows - currentRow + 1;
-  let right = rows + currentRow - 1;
+//   let current = i <= rows ? i : rows * 2 - i;
 
-  for (let j = 1; j <= rows * 2 - 1; j++) {
-    if (j >= left && j <= right) {
-      star += "*";
-    } else {
-      star += " ";
-    }
+//   for (let j = 1; j <= rows * 2 - 1; j++) {
+//     if (j === rows || (j >= rows - current + 1 && j <= rows + current - 1)) {
+//       star += "*";
+//     } else {
+//       star += " ";
+//     }
+//   }
+
+//   console.log(star);
+// }
+
+// const rows = 3;
+
+// for (let i = 1; i <= rows * 2 - 1; i++) {
+//   let star = "";
+
+//   const current = i <= rows ? i : rows * 2 - i;
+
+//   for (let j = 1; j <= rows * 2 - 1; j++) {
+//     if (j === rows||j>=rows-current+1&&j<=rows+current-1) {
+//       star += "*";
+//     } else {
+//       star += " ";
+//     }
+//   }
+//   console.log(star);
+// }
+
+// ***********************************************************************************************************
+
+// Print Hourglass Pattern
+
+// const row = 5;
+
+// for (let i = 1; i <= row * 2 - 1; i++) {
+//   let star = "";
+
+//   for (let j = row * 2 - 1; j >= 1; j--) {
+//     if (j >= i) {
+//       star += "*";
+//     } else {
+//       star += " ";
+//     }
+//   }
+
+//   console.log(star);
+// }
+
+// const rows = 5;
+
+// for (let i = rows * 2 - 1; i >= 1; i--) {
+//   let star = "";
+
+//   let current = i < rows ? rows - i + 1 : i - rows + 1;
+
+//   for (let j = 1; j <= rows * 2 - 1; j++) {
+//     if (j == rows || (j >= rows - current + 1 && j <= rows - 1 + current)) {
+//       star += "*";
+//     } else {
+//       star += " ";
+//     }
+//   }
+//   console.log(star);
+// }
+
+// let rows = 5;
+
+// for (let i = rows * 2 - 1; i >= 1; i--) {
+//   let star = "";
+
+//   let current = i < rows ? rows - i + 1 : i - rows + 1;
+
+//   for (let j = 1; j <= rows * 2 - 1; j++) {
+//     if (j >= rows - current + 1 && j <= rows + current - 1) {
+//       star += "*";
+//     } else {
+//       star += " ";
+//     }
+//   }
+
+//   console.log(star);
+// }
+
+// ***********************************************************************************************************
+
+// Print Hollow Diamond
+
+// const rows = 3;
+
+// for (let i = 1; i <= rows * 2 - 1; i++) {
+//   let star = "";
+
+//   let current = i <= rows ? i : rows * 2 - i;
+
+//   for (let j = 1; j <= rows * 2 - 1; j++) {
+//     if (j == rows - current + 1 || j == rows + current - 1) {
+//       star += "*";
+//     } else {
+//       star += " ";
+//     }
+//   }
+
+//   console.log(star);
+// }
+
+// ***********************************************************************************************************
+
+// Print Rhombus Pattern
+
+// const rows = 5;
+
+// for (let i = 1; i <= rows; i++) {
+//   let star = "";
+
+//   for (let j = 1; j <= rows * 2 - 1; j++) {
+//     if (j >= rows - i + 1 && j <= rows * 2 - i) {
+//       star += "*";
+//     } else {
+//       star += " ";
+//     }
+//   }
+
+//   console.log(star);
+// }
+
+// ***********************************************************************************************************
+
+// Print Multiplication Table (Single)
+
+// const number = 10;
+
+// for (let i = 1; i <= 10; i++) {
+//   console.log(`${number} x ${i} = ${number * i}`);
+// }
+
+// ***********************************************************************************************************
+
+// Print Multiplication Table (Upto N)
+
+const number = 5;
+
+for (let i = 1; i <= number; i++) {
+  console.log(`Table of ${i}`);
+
+  for (let j = 1; j <= 10; j++) {
+    console.log(`${i} x ${j} = ${i * j} `);
   }
 
-  console.log(star);
+  console.log("...................");
 }
-
